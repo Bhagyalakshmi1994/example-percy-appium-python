ca98b217d20882cdb17afb3851f83e32784d8152
Bhagyalakshmi1994 <bk20@ext.uber.com>
2025-07-15 17:55:41 +0530

    changes the text to java

--- tests/android.py
+++ tests/android.py
@@ -24,12 +24,13 @@ def run_session(capability):
             (AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")
         )
     )
-    search_input.send_keys("Python")
+    search_input.send_keys("java")
     time.sleep(2)
     driver.hide_keyboard()
     percy_screenshot(driver, 'screenshot 2')
 
     driver.quit()
+
 
 
 if __name__ == '__main__':
